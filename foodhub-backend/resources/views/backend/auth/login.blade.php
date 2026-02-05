bec02116423ef3396ddc89a9ca07fa05174346d1
proshehab <shehab.shakib@gmail.com>
2026-02-05 17:31:31 +0600

    refactor login form layout and remove required attributes from inputs

--- foodhub-backend/resources/views/backend/auth/login.blade.php
+++ foodhub-backend/resources/views/backend/auth/login.blade.php
@@ -1,29 +1,32 @@
 @extends('backend.layouts.app')
 
 @section('content')
-    <section class="login-section" id="login">
-        <div class="form-box login-form">
-            <h2> Admin Login</h2>
+<section class="login-section" id="login">
+    <div class="form-box login-form">
+        <h2> Admin Login</h2>
 
-            <form action="{{ route('admin.login.submit') }}" method="POST">
-                @csrf
+        <form action="{{ route('admin.login.submit') }}" method="POST">
+            @csrf
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" name="email" required>
-                </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email">
+            </div>
+            @error('email')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
 
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" required>
-                </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
 
-                <button type="submit" class="btn btn-primary">Login</button>
+            <button type="submit" class="btn btn-primary">Login</button>
 
-                <p class="form-link">
-                    Don't have an account? <a href="#">Register here</a>
-                </p>
-            </form>
-        </div>
-    </section>
+            <p class="form-link">
+                Don't have an account? <a href="#">Register here</a>
+            </p>
+        </form>
+    </div>
+</section>
 @endsection
